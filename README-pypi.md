220a0e03e19736400d4f2a130700d6a9ddfc1dfe
poco_cpp <pocopococpp198@gmail.com>
2025-05-11 00:30:41 +0900

    [vscode] change: pypi準備+ruffに対応

---
+++ README-pypi.md
@@ -0,0 +1,1 @@
+Please see [GitHub](https://github.com/pocokhc/GymBizHawk) for more details and usage examples.
--- gymbizhawk/__init__.py
+++ gymbizhawk/__init__.py
@@ -1,1 +1,1 @@
-from .version import VERSION as __version__  # noqa F401
+from .version import __version__  # noqa F401
